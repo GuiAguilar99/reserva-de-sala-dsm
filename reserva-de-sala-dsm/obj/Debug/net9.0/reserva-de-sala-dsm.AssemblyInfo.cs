9a3a784aa6eeb4b568ed08efc5a88d3cf3cfb5e0
GuiAguilar99 <gui_c_aguilar@hotmail.com>
2025-10-01 21:59:01 -0300

    First Commit

--- reserva-de-sala-dsm/obj/Debug/net9.0/reserva-de-sala-dsm.AssemblyInfo.cs
+++ reserva-de-sala-dsm/obj/Debug/net9.0/reserva-de-sala-dsm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("reserva-de-sala-dsm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbc964ecd20378e21b3f1ff450d4bb5525229f19")]
 [assembly: System.Reflection.AssemblyProductAttribute("reserva-de-sala-dsm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("reserva-de-sala-dsm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
